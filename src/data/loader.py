6eab6ea50387abeeacefce31d8f9f00cd38effc4
Bagusdevaa <deva999official@gmail.com>
2025-09-12 18:19:04 +0800

    Fix: Fix the import for call .env file

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,11 +1,14 @@
 import os
 import streamlit as st
 import pandas as pd
+from dotenv import load_dotenv
 import gspread
 from google.oauth2.service_account import Credentials
 
+load_dotenv()
+
 SHEET_NAME = os.getenv("SHEET_NAME", "[Silver]Unified_Clean_Data")
-GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")  # put in .env
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")  # put in .env
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets.readonly",
@@ -15,17 +18,17 @@ SCOPES = [
 @st.cache_data(show_spinner=False, ttl=600)
 def load_data() -> pd.DataFrame:
     """Load data from Google Sheets and return DataFrame."""
-    if not GOOGLE_SHEET_ID:
-        raise RuntimeError("GOOGLE_SHEET_ID env var missing")
+    if not SPREADSHEET_ID:
+        raise RuntimeError("SPREADSHEET_ID env var missing")
 
     # Credentials from service account json file path provided via env var or default
-    service_account_file = os.getenv("GOOGLE_SHEETS_CREDENTIALS_FILE", "google-credentials.json")
+    service_account_file = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "google-credentials.json")
     if not os.path.exists(service_account_file):
         raise FileNotFoundError(f"Service account file not found: {service_account_file}")
 
     credentials = Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
     client = gspread.authorize(credentials)
-    ws = client.open_by_key(GOOGLE_SHEET_ID).worksheet(SHEET_NAME)
+    ws = client.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
     rows = ws.get_all_records()
     df = pd.DataFrame(rows)
 
